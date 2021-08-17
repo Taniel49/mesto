6b01c7cc3cec3f44f650d9e0846e2fb479db7071
Mikhail Butyrskiy <laughingstorm49@gmail.com>
2021-08-17 04:40:10 +0300

    Михаил Бутырский, проектная работа 5, промежуточная версия

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,42 @@ let popupName = document.querySelector('.popup__inputs_type_name');
 let profileAbout= document.querySelector('.profile__about');
 let popupAbout = document.querySelector('.popup__inputs_type_about');
 let formElement = document.querySelector('form[name=profile-info]');
+const elementTemplate = document.querySelector('#element-template').content;
+const elements = document.querySelector('.elements');
+const elementName = document.querySelector('.element__name');
+const elementPicture = document.querySelector('.element__picture');
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+// Добавление элементов
+elements.append(elementTemplate);
+elementName.textContent='Архыз';
+elementPicture.setAttribute("src", "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg");
+
 
 // Открытие и закрытие попапа
 
